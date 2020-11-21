20ce187a1e1a08a39da9a4085ed0d5899d28b620
AryanMangale <aryan.a.mangale@gmail.com>
2020-11-21 08:29:22 +0100

    Functions for xor

---
+++ xor_utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def xor(a, b):
+    return int(a) ^ int(b)
+
+
+def xor_many(bit_array):
+    temp = 0
+    another_temp = 0
+    for i in range(0, len(bit_array)):
+        bit_val = bit_array[i]
+        another_temp = xor(temp, bit_val)
+        temp = another_temp
+    return temp

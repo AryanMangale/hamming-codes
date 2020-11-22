88cba28ad660e5cdb6039f8768cca4eeb7932bb8
Aryan Mangale <aryan.a.mangale@gmail.com>
2020-11-22 20:25:15 +0100

    Added modes parameter

--- check_block.py
+++ check_block.py
@@ -3,7 +3,7 @@
 from xor_utils import xor_many
 
 
-def check_block(block):
+def check_block(block, mode):
     p1 = xor_many([int(block[1]), block[5], block[9], block[13],
                    int(block[3]), block[7], block[11], block[15]])
     b1 = bool(p1 == 0)
@@ -20,4 +20,7 @@ def check_block(block):
                    block[13], block[9], block[11], block[15]])
     b8 = bool(p8 == 0)
 
-    return p1,b1,p2,b2,p4,b4,p8,b8
+    if mode=="debug":
+        return p1,b1,p2,b2,p4,b4,p8,b8
+    elif mode=="boolean"
+        return b1,b2,b4,b8

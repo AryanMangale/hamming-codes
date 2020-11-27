9124479e67af588e288223979ccc27752e70a9ce
Aryan Mangale <aryan.a.mangale@gmail.com>
2020-11-27 20:43:32 +0100

    Add function to check block

--- check_block.py
+++ check_block.py
@@ -3,7 +3,7 @@
 from xor_utils import xor_many
 
 
-def check_block(block, mode):
+def stats(block, mode):
     p1 = xor_many([int(block[1]), block[5], block[9], block[13],
                    int(block[3]), block[7], block[11], block[15]])
     b1 = bool(p1 == 0)
@@ -20,7 +20,15 @@ def check_block(block, mode):
                    block[13], block[9], block[11], block[15]])
     b8 = bool(p8 == 0)
 
-    if mode=="debug":
+    if mode=="DBUG":
         return p1,b1,p2,b2,p4,b4,p8,b8
-    elif mode=="boolean"
+    elif mode=="BOOL":
         return b1,b2,b4,b8
+
+
+def check(block):
+    real_block = block
+    (a,b,c,d) = stats(block, 'boolean')
+
+    return real_block
+    return a,b,c,d
